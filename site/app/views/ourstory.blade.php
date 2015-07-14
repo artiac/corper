a70424460abb9228e94591bda8a5a45dbb81504e
artiac <artichauhan290@gmail.com>
2015-07-14 15:25:33 +0530

    changes in ourstory page

--- site/app/views/ourstory.blade.php
+++ site/app/views/ourstory.blade.php
@@ -51,9 +51,9 @@
       <hr>
       <div class="row mix-grid thumbnails">
         <div id="owl-example" class="owl-carousel">
-             <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+             <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Initial-though.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Initial-though.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>The initial thought</h2>
                     <p>2014 April 25th</p>
@@ -61,9 +61,9 @@
                </div>           
             </div>                       
           </div>
-             <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+             <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Opening-Master-Plan-Documen.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Opening-Master-Plan-Documen.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Opening Master Plan Document</h2>
                     <p>2014 May 01st</p>
@@ -71,9 +71,9 @@
                </div>           
             </div>                       
           </div>
-           <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+           <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Figuring-out-the-Name.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Figuring-out-the-Name.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                     <h2>Figuring out the Name</h2>
                     <p>2014 May 04th</p>                 
@@ -81,9 +81,9 @@
                </div>           
             </div>                       
           </div>
-             <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+             <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/logo-creation.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/logo-creation.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Logo Creation begins</h2>
                     <p>2015 May 15th</p>
@@ -91,9 +91,9 @@
                </div>           
             </div>                       
           </div>
-             <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+             <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Initial-logo.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Initial-logo.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Initial Logo attempt</h2>
                     <p>2014 May 25th</p>
@@ -101,9 +101,9 @@
                </div>           
             </div>                       
           </div>
-           <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+           <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Facebook-Page1.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Facebook-Page1.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Facebook Page opened</h2>
                   <p>2014 Sept 8th</p>                 
@@ -111,9 +111,9 @@
                </div>           
             </div>                       
           </div> 
-             <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+             <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/time-to-eat1.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/time-to-eat1.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>No time for proper meal</h2>
                   <p>2014 Sept 19th</p>                 
@@ -121,9 +121,9 @@
                </div>           
             </div>                       
           </div>
-           <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+           <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Founders1.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Founders1.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Founders POP ()</h2>
                   <p>2014 Oct 16th</p>                 
@@ -131,9 +131,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/finalizing.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/finalizing.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Finalising master plan</h2>
                     <p>2014 Oct 22nd - 2014 Oct 22th</p>
@@ -141,9 +141,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/master-plan.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/master-plan.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Master Plan Completed</h2>
                     <p>2014 Oct 22nd</p>
@@ -151,9 +151,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/corper-logo-creation.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/corper-logo-creation.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Corperlife logo creation process</h2>
                     <p>2015 Jan 26th</p>
@@ -161,9 +161,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Final-logo.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Final-logo.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                   <h2>Final logo</h2>
                     <p>2015 Jan 26th</p>
@@ -171,9 +171,9 @@
                </div>           
             </div>                       
           </div>         
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/Working-on-checklists.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/Working-on-checklists.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                  <h2>Working on checklists</h2>
                  <p>2015 Jan 14th</p>                 
@@ -181,9 +181,9 @@
                </div>           
             </div>                       
           </div>
-         <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+         <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/first-fb-page.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/first-fb-page.jpg" class="img-rounded max-height-300 width-100 min-height-300">
                <div class="mix-details">
                  <h2>First Facebook Post</h2>
                  <p>2015 Feb 20th</p>                 
@@ -191,9 +191,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/first-temp.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/first-temp.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                  <h2>First Temp website up!</h2>
                  <p>2015 Apr 09th</p>                 
@@ -201,9 +201,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/corper-ignore.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/corper-ignore.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                  <h2>Corperlife Ignored</h2>
                  <p>2015 Apr 09th</p>                 
@@ -211,9 +211,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/launched.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/launched.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                  <h2>The Bugle (by corperlife™) begins</h2>
                  <p>2015 Apr 17th</p>                 
@@ -221,9 +221,9 @@
                </div>           
             </div>                       
           </div>
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/corperlife-free.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/corperlife-free.jpg" class="img-rounded width-100 min-height-300">
                <div class="mix-details">
                  <h2>Corperlife Free Camp guide released</h2>
                  <p>2015 Apr 25th</p>                 
@@ -231,9 +231,9 @@
                </div>           
             </div>                       
           </div>       
-          <div class="mix category_1 mix_all" style="display: block;  opacity: 1; padding:20px 20px 0px 20px;">
+          <div class="mix category_1 mix_all">
             <div class="mix-inner">
-               <img alt="" src="assets/img/story/reached-100.jpg" class="img-rounded" style="width:100%">
+               <img alt="" src="assets/img/story/reached-100.jpg" class="img-rounded max-height-300 width-100 min-height-300">
                <div class="mix-details">
                   <h2>Reached 1000 Facebook Likes!</h2>
                   <p>2015 June 06th</p>                 
